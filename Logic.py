e73a8891351bb6be5c2ea91b13d86a08d24478fa
David Johnson <85799984+David-Johnson-0@users.noreply.github.com>
2024-05-10 15:17:16 -0500

    Update Logic.py

--- Logic.py
+++ Logic.py
@@ -4,10 +4,13 @@ from gui.home import *
 from gui.dep import *
 from gui.wit import *
 from PyQt6.QtWidgets import *
+import hashlib
+import re
 
 class Logic(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.account = None
 
     def show_log(self):
         self.login = LogLogic(self)
@@ -43,6 +46,13 @@ class Logic(QMainWindow):
         if self.centralWidget() is not None:
             self.centralWidget().close()
 
+    def hash_password(self, password):
+        password_bytes = password.encode('utf-8')
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(password_bytes)
+        hashed_pass = sha256_hash.hexdigest()
+        return hashed_pass
+
 
 class LogLogic(QMainWindow, Ui_LoginDisplay):
     def __init__(self, Logic):
@@ -55,23 +65,84 @@ class LogLogic(QMainWindow, Ui_LoginDisplay):
         pass
 
     def validate(self):
-        self.logic.show_home()
-        pass
+        email = self.usernameinput.text()
+        with open('files/login_info.txt', 'r') as file:
+            for line in file:
+                if line.startswith(email):
+                    content = line.split()
+                    if content[1] == self.logic.hash_password(self.pwinput.text()):
+                        self.logic.account = content[0]
+                        self.logic.show_home()
+                    else:
+                        self.pwinput.clear()
+                        self.usernameinput.clear()
+                        self.setfocus(self.usernameinput)
+
 class RegLogic(QMainWindow, Ui_Reg):
     def __init__(self, Logic):
         super().__init__(Logic)
         self.setupUi(self)
         self.logic = Logic
 
-        self.SubmitButton.clicked.connect(lambda: self.logic.show_log())
+        self.SubmitButton.clicked.connect(lambda: self.register())
         self.BackButton.clicked.connect(lambda: self.logic.show_log())
         pass
+
+    def register(self):
+        email = self.EmailInput.text()
+        pass1 = self.PWInput.text()
+        pass2 = self.PWInput2.text()
+        if self.val_inputs():
+            if pass1 == pass2:
+                with open('files/login_info.txt', 'a') as file:
+                    file.write(f'{email} {self.logic.hash_password(pass1)} {self.DOBInput.text()} {self.FNameInput.text()} {self.LNameInput.text()}\n')
+                    self.logic.show_log()
+            else:
+                self.PWInput.setText(None)
+                self.PWInput2.setText(None)
+                self.EmailInput.setText(None)
+                self.DOBInput.setText(None)
+                self.LNameInput.setText(None)
+                self.FNameInput.setText(None)
+                self.setfocus(self.FNameInput)
+        else:
+            pass
+
+    def val_inputs(self):
+        if self.is_empty(self.PWInput.text()):
+            return False
+        if self.is_empty(self.PWInput2.text()):
+            return False
+        if self.is_empty(self.LNameInput.text()):
+            return False
+        if self.is_empty(self.FNameInput.text()):
+            return False
+        if self.is_empty(self.EmailInput.text()):
+            return False
+        if self.is_empty(self.DOBInput.text()):
+            return False
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+"
+        if not re.match(pattern, self.EmailInput.text()):
+            return False
+        return True
+
+    def is_empty(self, name):
+        if name == "":
+            return True
+        else:
+            return False
 
 class HomeLogic(QMainWindow, Ui_Home):
     def __init__(self, Logic):
         super().__init__(Logic)
         self.setupUi(self)
         self.logic = Logic
+        with open("files/login_info.txt", 'r') as file:
+            for line in file:
+                if line.startswith(self.logic.account):
+                    row = line.split()
+                    self.SavingButton.setText(f'{row[3]}\'s Saving Account')
+                    self.CheckButton.setText(f'{row[3]}\'s Checking Account')
 
         self.SignOutButton.clicked.connect(lambda: self.logic.show_log())
         self.WDButton.clicked.connect(lambda: self.logic.show_wit())
