a5cfa4a43e64aae34171085b7ab7a4169668dcbd
David Johnson <85799984+David-Johnson-0@users.noreply.github.com>
2024-05-10 17:13:54 -0500

    Update Logic.py

--- Logic.py
+++ Logic.py
@@ -1,4 +1,3 @@
-# Imports the gui files and their content as well as the needed libraries
 from gui.log import *
 from gui.reg import *
 from gui.home import *
@@ -7,47 +6,48 @@ from gui.wit import *
 from PyQt6.QtWidgets import *
 import hashlib
 import re
+import csv
 
 class Logic(QMainWindow):
-    # Initialization function
     def __init__(self) -> None:
         super().__init__()
         self.account = None
-    # Opens login screen
+        self.type = ''
+
     def show_log(self) -> None:
         self.login = LogLogic(self)
         self.close_page()
         self.setCentralWidget(self.login)
         self.login.show()
-    # Opens registration screen
+
     def show_reg(self) -> None:
         self.reg = RegLogic(self)
         self.close_page()
         self.setCentralWidget(self.reg)
         self.reg.show()
-    # Opens withdraw screen
+
     def show_wit(self) -> None:
         self.wit = WitLogic(self)
         self.close_page()
         self.setCentralWidget(self.wit)
         self.wit.show()
-    # Opens deposit screen
+
     def show_dep(self) -> None:
         self.dep = DepLogic(self)
         self.close_page()
         self.setCentralWidget(self.dep)
         self.dep.show()
-    # Opens home screen
+
     def show_home(self) -> None:
         self.home = HomeLogic(self)
         self.close_page()
         self.setCentralWidget(self.home)
         self.home.show()
-    # Closes the current page
+
     def close_page(self) -> None:
         if self.centralWidget() is not None:
             self.centralWidget().close()
-    # Hashes the password of the user
+
     def hash_password(self, password) -> str:
         password_bytes = password.encode('utf-8')
         sha256_hash = hashlib.sha256()
@@ -57,7 +57,6 @@ class Logic(QMainWindow):
 
 
 class LogLogic(QMainWindow, Ui_LoginDisplay):
-    # Initializes the UI and establishes connections.
     def __init__(self, Logic) -> None:
         super().__init__(Logic)
         self.setupUi(self)
@@ -66,7 +65,7 @@ class LogLogic(QMainWindow, Ui_LoginDisplay):
         self.RegisterButton.clicked.connect(lambda: self.logic.show_reg())
         self.SubmitButton.clicked.connect(lambda: self.validate())
         pass
-    # Reads the email and validates that the email is in the text file (a registered user).
+
     def validate(self) -> None:
         email = self.usernameinput.text()
         with open('files/login_info.txt', 'r') as file:
@@ -79,10 +78,9 @@ class LogLogic(QMainWindow, Ui_LoginDisplay):
                     else:
                         self.pwinput.clear()
                         self.usernameinput.clear()
-                        self.setfocus(self.usernameinput)
+                        self.usernameinput.setFocus()
 
 class RegLogic(QMainWindow, Ui_Reg):
-    # Initializes screen and establshes connections
     def __init__(self, Logic) -> None:
         super().__init__(Logic)
         self.setupUi(self)
@@ -91,8 +89,6 @@ class RegLogic(QMainWindow, Ui_Reg):
         self.SubmitButton.clicked.connect(lambda: self.register())
         self.BackButton.clicked.connect(lambda: self.logic.show_log())
         pass
-    # Takes in the email and password the user enters and, if the passwords match, adds them to the text file.
-    # Otherwise, the inputs are reset and the user must input again.
     def register(self) -> None:
         email = self.EmailInput.text()
         pass1 = self.PWInput.text()
@@ -101,6 +97,7 @@ class RegLogic(QMainWindow, Ui_Reg):
             if pass1 == pass2:
                 with open('files/login_info.txt', 'a') as file:
                     file.write(f'{email} {self.logic.hash_password(pass1)} {self.DOBInput.text()} {self.FNameInput.text()} {self.LNameInput.text()}\n')
+                    self.gen_csv()
                     self.logic.show_log()
             else:
                 self.PWInput.setText(None)
@@ -112,8 +109,7 @@ class RegLogic(QMainWindow, Ui_Reg):
                 self.setfocus(self.FNameInput)
         else:
             pass
-    # Validates the inputs, ensuring the formatting is accurate and the passwords are valid.
-    def val_inputs(self) -> Boolean:
+    def val_inputs(self) -> bool:
         if self.is_empty(self.PWInput.text()):
             return False
         if self.is_empty(self.PWInput2.text()):
@@ -130,15 +126,19 @@ class RegLogic(QMainWindow, Ui_Reg):
         if not re.match(pattern, self.EmailInput.text()):
             return False
         return True
-    # Checks if input is empty.
-    def is_empty(self, name) -> Boolean:
+    def is_empty(self, name) -> bool:
         if name == "":
             return True
         else:
             return False
+    def gen_csv(self) -> None:
+        with open('files/account_info.csv', mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([f'{self.EmailInput.text()}-Save', 0])
+            writer.writerow([f'{self.EmailInput.text()}-Check', 0])
+
 
 class HomeLogic(QMainWindow, Ui_Home):
-    # Initializes home screen and establishes button connections (where the user goes with each button
     def __init__(self, Logic) -> None:
         super().__init__(Logic)
         self.setupUi(self)
@@ -149,28 +149,73 @@ class HomeLogic(QMainWindow, Ui_Home):
                     row = line.split()
                     self.SavingButton.setText(f'{row[3]}\'s Saving Account')
                     self.CheckButton.setText(f'{row[3]}\'s Checking Account')
-
         self.SignOutButton.clicked.connect(lambda: self.logic.show_log())
+        self.WDButton.clicked.connect(lambda: self.get_checked())
         self.WDButton.clicked.connect(lambda: self.logic.show_wit())
+        self.DepButton.clicked.connect(lambda: self.get_checked())
         self.DepButton.clicked.connect(lambda: self.logic.show_dep())
         pass
 
+    def get_checked(self) -> None:
+        if self.SavingButton.isChecked():
+            self.logic.type = '-Save'
+        elif self.CheckButton.isChecked():
+            self.logic.type = '-Check'
+        else:
+            pass
+
 class DepLogic(QMainWindow, Ui_DepWindow):
-    # Initializes deposit page
     def __init__(self, Logic) -> None:
         super().__init__(Logic)
         self.setupUi(self)
         self.logic = Logic
-
+        self.DepButton.clicked.connect(lambda: self.deposit())
         self.BackButton.clicked.connect(lambda: self.logic.show_home())
         pass
 
+    def deposit(self) -> None:
+        with open('files/account_info.csv', mode='r', newline='') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            for row in rows:
+                if f'{row[0]}' == f'{self.logic.account}{self.logic.type}':
+                    if self.DepLabel.text() == self.DepInput2.text():
+                        row[1] = int(row[1]) + int(self.DepInput2.text())
+                        self.DepLabel.setText('')
+                        self.DepLabel.setPlaceholderText(f'${row[1]}')
+                        break
+
+        with open('files/account_info.csv', mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(rows)
+        self.DepInput2.setText('')
+
 class WitLogic(QMainWindow, Ui_Withdraw):
-    # Initializes withdraw page
     def __init__(self, Logic) -> None:
         super().__init__(Logic)
         self.setupUi(self)
         self.logic = Logic
-
+        self.WitButton.clicked.connect(lambda: self.withdraw())
         self.BackButton_2.clicked.connect(lambda: self.logic.show_home())
+        pass
+    def withdraw(self) -> None:
+        with open('files/account_info.csv', mode='r', newline='') as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+            for row in rows:
+                if f'{row[0]}' == f'{self.logic.account}{self.logic.type}':
+                    if self.WitLabel_2.text() == self.WitInput2.text():
+                        if (int(row[1]) - int(self.WitInput2.text())) > 0:
+                            row[1] = int(row[1]) - int(self.WitInput2.text())
+                            self.WitLabel_2.setText('')
+                            self.WitLabel_2.setPlaceholderText(f'${row[1]}')
+                            allow = True
+                        else:
+                            allow = False
+                            continue
+        if allow:
+            with open('files/account_info.csv', mode='w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(rows)
+        self.WitInput2.setText('')
         pass
